10f1d40f1cb60f884a01c07fb826febdda4ca261
jong-jang <jjh0299@gmail.com>
2023-07-09 10:19:32 +0900

    모달창

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,7 +1,11 @@
-import React from 'react';
-
-function Modal() {
-	return <div>Modal</div>;
-}
+const Modal = () => {
+	return (
+		<aside className='modal'>
+			<div className='con'>
+				<span className='close'>close</span>
+			</div>
+		</aside>
+	);
+};
 
 export default Modal;
