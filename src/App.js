cac2cdc6860e0c96eb88a27c5781e6c5277471c1
jong-jang <jjh0299@gmail.com>
2023-07-15 09:21:04 +0900

    Main 임포트, Header render 적용

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Footer from './components/common/Footer';
 import Header from './components/common/Header';
 
-import Banner from './components/main/Banner';
-import Btns from './components/main/Btns';
-import News from './components/main/News';
-import Pics from './components/main/Pics';
-import Vids from './components/main/Vids';
-import Visual from './components/main/Visual';
+import Main from './components/main/Main';
 
 import Community from './components/sub/Community';
 import Contact from './components/sub/Contact';
@@ -23,19 +18,8 @@ function App() {
 		<>
 			{/* Switch내부에 중복되는 라우트 경로가 있을때 먼저나온 라우트를 채택하고 그 이후는 무시 */}
 			<Switch>
-				<Route exact path='/'>
-					<Header type={'main'} />
-					<Visual />
-					<News />
-					<Pics />
-					<Vids />
-					<Banner />
-					<Btns />
-				</Route>
-
-				<Route path='/'>
-					<Header type={'sub'} />
-				</Route>
+				<Route exact path='/' component={Main} />
+				<Route path='/' render={() => <Header type={'sub'} />} />
 			</Switch>
 
 			<Route path='/department' component={Department} />
