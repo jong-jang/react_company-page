1e012cd647051f2872831fce36fed2ba949a8fc7
jong-jang <jjh0299@gmail.com>
2023-07-15 11:40:06 +0900

    scroll state

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -1,7 +1,8 @@
-function Pics() {
+function Pics({ Scrolled }) {
+	console.log(Scrolled);
 	return (
 		<section id='pics' className='myScroll'>
-			<h1>Flicker</h1>
+			<h1>FLICKR</h1>
 		</section>
 	);
 }
