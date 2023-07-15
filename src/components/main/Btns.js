1e57bb53364c410e5441df7e28d36b6b0d1f5d1a
jong-jang <jjh0299@gmail.com>
2023-07-15 10:11:12 +0900

    getPos, 윈도우 객체 이벤트 Btns 컴포넌트로 이동

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -1,6 +1,25 @@
+import { useEffect, useRef } from 'react';
+
 function Btns() {
+	const btnRef = useRef(null);
+	let pos = useRef([]);
+
+	const getPos = () => {
+		pos.current = [];
+		const secs = btnRef.current.parentElement.querySelectorAll('.myScroll');
+		for (const sec of secs) pos.current.push(sec.offsetTop);
+		console.log(pos.current);
+	};
+
+	useEffect(() => {
+		getPos();
+		window.addEventListener('resize', getPos);
+		return () => {
+			window.removeEventListener('resize', getPos);
+		};
+	}, []);
 	return (
-		<ul id='scroll_navi'>
+		<ul id='scroll_navi' ref={btnRef}>
 			<li></li>
 			<li></li>
 			<li></li>
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -6,29 +6,9 @@ import Pics from './Pics';
 import Vids from './Vids';
 import Visual from './Visual';
 
-import { useEffect, useRef } from 'react';
-
 function Main() {
-	const main = useRef(null);
-	let pos = useRef([]);
-
-	const getPos = () => {
-		pos.current = [];
-		const secs = main.current.querySelectorAll('.myScroll');
-		for (const sec of secs) pos.current.push(sec.offsetTop);
-		console.log(pos.current);
-	};
-
-	useEffect(() => {
-		getPos();
-		window.addEventListener('resize', getPos);
-		return () => {
-			window.removeEventListener('resize', getPos);
-		};
-	}, []);
-
 	return (
-		<main ref={main}>
+		<main>
 			<Header type={'main'} />
 			<Visual />
 			<News />
