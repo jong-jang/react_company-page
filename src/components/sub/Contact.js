a9555b5e98ccf1d0e858b7bf1f42d12fb618c3fb
jong-jang <jjh0299@gmail.com>
2023-07-16 11:06:06 +0900

    지도정보 여러개 버튼 클릭시 변경

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -6,21 +6,40 @@ function Contact() {
 	const [Traffic, setTraffic] = useState(false);
 	const container = useRef(null);
 	const mapInstanceRef = useRef(null);
-	const option = useRef({
-		center: new kakao.maps.LatLng(37.55554565158597, 126.9673777368895),
-		level: 3,
-	});
-
+	const [Index, setIndex] = useState(0);
+	const info = useRef([
+		{
+			title: '삼성역 코엑스',
+			latlng: new kakao.maps.LatLng(37.51100661425726, 127.06162026853143),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+		{
+			title: '넥슨 본사',
+			latlng: new kakao.maps.LatLng(37.40211707077346, 127.10344953763003),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker2.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+		{
+			title: '서울 시청',
+			latlng: new kakao.maps.LatLng(37.5662952, 126.9779451),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+	]);
 	useEffect(() => {
-		const mapInstance = new kakao.maps.Map(container.current, option.current);
-		const markerImage = new kakao.maps.MarkerImage(`${process.env.PUBLIC_URL}/img/marker1.png`, new kakao.maps.Size(232, 99), { offset: new kakao.maps.Point(116, 99) });
+		const mapInstance = new kakao.maps.Map(container.current, { center: info.current[Index].latlng, level: 3 });
+		const markerImage = new kakao.maps.MarkerImage(info.current[Index].imgSrc, info.current[Index].imgSize, info.current[Index].imgPos);
 		const marker = new kakao.maps.Marker({
-			position: option.current.center,
-			image: markerImage, // 마커이미지 설정
+			position: info.current[Index].latlng,
+			image: markerImage,
 		});
 		marker.setMap(mapInstance);
 		mapInstanceRef.current = mapInstance;
-	}, []);
+	}, [Index]);
 
 	useEffect(() => {
 		Traffic ? mapInstanceRef.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : mapInstanceRef.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
@@ -36,6 +55,21 @@ function Contact() {
 			>
 				{Traffic ? '교통정보 끄기' : '교통정보 보기'}
 			</button>
+
+			<ul>
+				{info.current.map((el, idx) => {
+					return (
+						<li
+							key={idx}
+							onClick={() => {
+								setIndex(idx);
+							}}
+						>
+							{el.title}
+						</li>
+					);
+				})}
+			</ul>
 		</Layout>
 	);
 }
