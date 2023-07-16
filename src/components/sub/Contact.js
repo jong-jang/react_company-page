b1651bfcc4c34ed3797f8f6e8a8a56516d486976
jong-jang <jjh0299@gmail.com>
2023-07-16 09:28:31 +0900

    kakao map 추가

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -1,7 +1,24 @@
 import Layout from '../common/Layout';
+import { useRef, useEffect } from 'react';
 
 function Contact() {
-	return <Layout name={'Contact'}>Contact</Layout>;
+	const container = useRef(null);
+	const { kakao } = window;
+	const option = useRef({
+		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		level: 3,
+	});
+
+	useEffect(() => {
+		var map = new kakao.maps.Map(container.current, option.current);
+		return () => {};
+	}, []);
+
+	return (
+		<Layout name={'Contact'}>
+			<div id='map' ref={container}></div>
+		</Layout>
+	);
 }
 
 export default Contact;
