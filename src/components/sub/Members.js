5d14e5747402e8b0efa56a87c0c036ede676f459
jong-jang <jjh0299@gmail.com>
2023-07-22 09:40:04 +0900

    input 요소 onchange 추가

--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -1,46 +1,74 @@
+import { useEffect, useState } from 'react';
 import Layout from '../common/Layout';
 
 function Members() {
+	const initVal = {
+		userid: '',
+		pwd1: '',
+		pwd2: '',
+		email: '',
+	};
+	const [Val, setVal] = useState(initVal);
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setVal({ ...Val, [name]: value });
+	};
+
+	useEffect(() => {
+		console.log(Val);
+	}, [Val]);
 	return (
 		<Layout name={'Members'}>
 			<form>
 				<fieldset>
-					<legend>회원가입 폼 양식</legend>
+					<legend className='h'>회원 가입 폼 양식</legend>
 					<table border='1'>
 						<tbody>
 							{/* userid */}
 							<tr>
 								<th scope='row'>
 									<label htmlFor='userid'>USER ID</label>
-									<input type='text' name='username' placeholder='아이디를 입력하세요' />
 								</th>
+								<td>
+									<input type='text' name='userid' placeholder='아이디를 입력하세요' value={Val.userid} onChange={handleChange} />
+								</td>
 							</tr>
+
 							{/* password */}
 							<tr>
 								<th>
 									<label htmlFor='pwd1'>PASSWORD</label>
-									<input type='password' name='pwd1' placeholder='비밀번호를 입력하세요' />
 								</th>
+								<td>
+									<input type='password' name='pwd1' placeholder='비밀번호를 입력하세요' value={Val.pwd1} onChange={handleChange} />
+								</td>
 							</tr>
+
 							{/* re password */}
 							<tr>
 								<th>
 									<label htmlFor='pwd2'>RE-PASSWORD</label>
-									<input type='password' name='pwd2' placeholder='비밀번호를 재입력하세요' />
 								</th>
+								<td>
+									<input type='password' name='pwd2' placeholder='비밀번호를 재입력하세요' value={Val.pwd2} onChange={handleChange} />
+								</td>
 							</tr>
+
 							{/* email */}
 							<tr>
 								<th>
 									<label htmlFor='email'>EMAIL</label>
-									<input type='text' name='pwd2' placeholder='이메일주소를 입력하세요' />
 								</th>
+								<td>
+									<input type='text' name='email' placeholder='이메일주소를 입력하세요' value={Val.email} onChange={handleChange} />
+								</td>
 							</tr>
 
 							{/* btnSet */}
 							<tr>
-								<th colSpan={2}>
-									<input type='reset' vlaue='cancel' />
+								<th colSpan='2'>
+									<input type='reset' value='CANCEL' />
 
 									<input type='submit' value='SEND' />
 								</th>
