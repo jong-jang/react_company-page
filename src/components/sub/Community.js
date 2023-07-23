bd7e14ab3c262dd867dfbb20a4eca558100bba56
jong-jang <jjh0299@gmail.com>
2023-07-23 09:41:30 +0900

    댓글 저장

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,12 +1,51 @@
+import { useEffect, useRef, useState } from 'react';
 import Layout from '../common/Layout';
 
 function Community() {
+	const input = useRef(null);
+	const textarea = useRef(null);
+	const [Posts, setPosts] = useState([]);
+
+	const resetForm = () => {
+		input.current.value = '';
+		textarea.current.value = '';
+	};
+
+	const createPost = () => {
+		if (!input.current.value.trim() || !textarea.current.value.trim()) {
+			return alert('제목과 본문을 모두 입력하세요');
+		}
+		setPosts([{ title: input.current.value, content: textarea.current.value }, ...Posts]);
+		alert('저장이 완료되었습니다.');
+		resetForm();
+	};
+
+	useEffect(() => {
+		console.log(Posts);
+	}, [Posts]);
+
 	return (
-		<Layout name={'Community'} bg={process.env.PUBLIC_URL + '/img/Community.jpg'}>
-			{/* 공통의 컴포넌트 안쪽의 자식으로 입력한 children이라는 props명으로 자동으로 Layout컴포넌트에 전달 */}
-			<p>Community 페이지 내용입니다</p>
+		<Layout name={'Community'}>
+			<div className='inputBox'>
+				<input type='text' placeholder='제목을 입력하세요' ref={input} />
+				<br />
+				<textarea name='30' rows='3' placeholder='본문을 입력하세요' ref={textarea}></textarea>
+				<br />
+
+				<button>cancel</button>
+				<button onClick={createPost}>write</button>
+			</div>
 		</Layout>
 	);
 }
 
 export default Community;
+
+/* 
+	Create - 데이터저장 (게시글 저장)
+	Read 	 - 데이터호출 (게시글 보기)
+	Update - 데이터수정 (게시글 수정)
+	Delete - 데이터삭제 (게시글 삭제)
+
+	localStorage: 모든 브라우저가 가지고 있는 경량의 저장소 (문자열 : 5MB)
+*/
