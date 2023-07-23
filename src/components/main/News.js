da42eda89fffb6ef860a13490d7466723dc3a69c
jong-jang <jjh0299@gmail.com>
2023-07-23 11:46:04 +0900

    미리보기 형식 추가

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 function News() {
+	const getlocalData = () => {
+		const data = localStorage.getItem('post');
+		return JSON.parse(data);
+	};
+
+	const [Posts] = useState(getlocalData());
 	return (
 		<section id='news' className='myScroll'>
-			<article></article>
-			<article></article>
-			<article></article>
-			<article></article>
+			{Posts.map((post, idx) => {
+				if (idx >= 4) return null;
+				return (
+					<article key={idx}>
+						<h3>{post.title}</h3>
+						<p>{post.content}</p>
+					</article>
+				);
+			})}
 		</section>
 	);
 }
