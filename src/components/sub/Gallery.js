22ef0178b60019513c7d9b68bd5bb873d14ffe0b
jong-jang <jjh0299@gmail.com>
2023-07-29 10:23:32 +0900

    이미지 프로필 추가/ 엑박시 대체프로필 이미지 출력

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,19 +1,31 @@
 import Layout from '../common/Layout';
 import axios from 'axios';
+import Masonry from 'react-masonry-component';
 import { useState, useEffect } from 'react';
 
 function Gallery() {
-	const key = '540e875989ee5c74090556f957686df1';
-	const num = 20;
-	const baseURL = `https://www.flickr.com/services/rest/?format=json&nojsoncallback=1&api_key=${key}&per_page=${num}`;
-	const method_interest = 'flickr.interestingness.getList';
-	const url_interest = `${baseURL}&method=${method_interest}`;
 	const [Items, setItems] = useState([]);
 
+	const getFlickr = async (opt) => {
+		const key = '540e875989ee5c74090556f957686df1';
+		const num = 20;
+		const baseURL = `https://www.flickr.com/services/rest/?format=json&nojsoncallback=1&api_key=${key}&per_page=${num}`;
+		const method_interest = 'flickr.interestingness.getList';
+		const method_search = 'flickr.photos.search';
+		const method_user = 'flickr.people.getPhotos';
+		let url = '';
+		if (opt.type === 'interest') url = `${baseURL}&method=${method_interest}`;
+		if (opt.type === 'search') url = `${baseURL}&method=${method_search}&tags=${opt.tags}`;
+		if (opt.type === 'user') url = `${baseURL}&method=${method_user}&user_id=${opt.user}`;
+
+		const result = await axios.get(url);
+		setItems(result.data.photos.photo);
+	};
+
 	useEffect(() => {
-		axios.get(url_interest).then((json) => {
-			setItems(json.data.photos.photo);
-		});
+		getFlickr({ type: 'interest' });
+		//getFlickr({ type: 'search', tags: 'landscape' });
+		//getFlickr({ type: 'user', user: '198837106@N07' });
 	}, []);
 
 	useEffect(() => {
@@ -21,23 +33,37 @@ function Gallery() {
 	}, [Items]);
 
 	return (
-		<Layout name='Gallery'>
+		<Layout name={'Gallery'}>
 			<div className='frame'>
-				{Items.map((item, idx) => {
-					return (
-						<article key={item.id}>
-							<div className='inner'>
-								<div className='pic'>
-									<img src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`} alt={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg`} />
+				<Masonry elementType={'div'} options={{ transitionDuration: '0.5s' }}>
+					{Items.map((item) => {
+						return (
+							<article key={item.id}>
+								<div className='inner'>
+									<div className='pic'>
+										<img src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`} alt={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg`} />
+									</div>
+									<h2>{item.title}</h2>
+
+									<div className='profile'>
+										<img
+											src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
+											alt={item.owner}
+											onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
+										/>
+										<span>{item.owner}</span>
+									</div>
 								</div>
-								<h2>{item.title}</h2>
-							</div>
-						</article>
-					);
-				})}
+							</article>
+						);
+					})}
+				</Masonry>
 			</div>
 		</Layout>
 	);
 }
 
 export default Gallery;
+
+// 540e875989ee5c74090556f957686df1
+// 198837106@N07
