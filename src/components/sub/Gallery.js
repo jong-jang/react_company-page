a1aac34e8df60a72f8f3e0eb56adf847a6d25546
jong-jang <jjh0299@gmail.com>
2023-07-29 09:33:53 +0900

    flickr api 출력

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,7 +1,43 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function Gallery() {
-	return <Layout name='Gallery'>Gallery</Layout>;
+	const key = '540e875989ee5c74090556f957686df1';
+	const num = 20;
+	const baseURL = `https://www.flickr.com/services/rest/?format=json&nojsoncallback=1&api_key=${key}&per_page=${num}`;
+	const method_interest = 'flickr.interestingness.getList';
+	const url_interest = `${baseURL}&method=${method_interest}`;
+	const [Items, setItems] = useState([]);
+
+	useEffect(() => {
+		axios.get(url_interest).then((json) => {
+			setItems(json.data.photos.photo);
+		});
+	}, []);
+
+	useEffect(() => {
+		console.log(Items);
+	}, [Items]);
+
+	return (
+		<Layout name='Gallery'>
+			<div className='frame'>
+				{Items.map((item, idx) => {
+					return (
+						<article key={item.id}>
+							<div className='inner'>
+								<div className='pic'>
+									<img src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`} alt={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg`} />
+								</div>
+								<h2>{item.title}</h2>
+							</div>
+						</article>
+					);
+				})}
+			</div>
+		</Layout>
+	);
 }
 
 export default Gallery;
