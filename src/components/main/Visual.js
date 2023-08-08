6228b4ec4925ae63369adcac8dff8277d9534277
jong-jang <jjh0299@gmail.com>
2023-08-08 14:35:40 +0900

    비쥬얼 영역 구조 추가

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -2,9 +2,22 @@ import { memo } from 'react';
 
 function Visual() {
 	return (
-		<figure id='visual' className='myScroll'>
-			<video src={`${process.env.PUBLIC_URL}/img/vid.mp4`} muted loop autoPlay></video>
-		</figure>
+		<div id='visual' className='myScroll'>
+			<div id="stars"></div>
+			<div id="stars2"></div>
+			<div id="stars3"></div>
+			<div id='title'>
+        <span>
+          Hello !
+        </span>
+        <br />
+        <span>
+          Welcome to <span>J</span> World
+        </span>
+      </div>
+      <div className="h_box"><img src={`${process.env.PUBLIC_URL}/img/hum.png`} alt="사람" /></div>
+      <div className="scroll_move"></div>
+		</div>
 	);
 }
 
