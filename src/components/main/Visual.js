2a8b3ad64dba2300bf3cdd3715d4a6578e6b9620
jong-jang <jjh0299@gmail.com>
2023-08-09 16:30:10 +0900

    비쥬얼 수정

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -6,17 +6,7 @@ function Visual() {
 			<div id="stars"></div>
 			<div id="stars2"></div>
 			<div id="stars3"></div>
-			<div id='title'>
-        <span>
-          Hello !
-        </span>
-        <br />
-        <span>
-          Welcome to <span>J</span> World
-        </span>
-      </div>
       <div className="h_box"><img src={`${process.env.PUBLIC_URL}/img/hum.png`} alt="사람" /></div>
-      <div className="scroll_move"></div>
 		</div>
 	);
 }
