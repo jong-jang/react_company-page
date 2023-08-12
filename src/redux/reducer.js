5c90b446515c49a5f29fa756f5b7175dce317737
jong-jang <jjh0299@gmail.com>
2023-08-12 10:03:34 +0900

    reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -35,7 +35,7 @@ const initMember = {
 	],
 };
 
-// 초기 state값을 지정하고 추후 action객체가 넘어오면 action타입에 따라 전역 state값을 변경해주는 변형자 함수
+//초기 state값을 지정하고 추후 action객체가 넘어오면 action타입에 따라 전역 state값을 변경해주는 변형자 함수
 const memberReducer = (state = initMember, action) => {
 	switch (action.type) {
 		case 'SET_MEMBERS':
@@ -45,6 +45,6 @@ const memberReducer = (state = initMember, action) => {
 	}
 };
 
-// 해당 변형자함수가 반환하는 리턴값을 하나의 객체로 합쳐서 외부로 export
-const reducers = combineReducers({ memberReducer });
-export default reducers;
+//해당 변형자함수가 반환하는 리턴값을 하나의 객체로 합쳐서 외부로 export
+const reducer = combineReducers({ memberReducer });
+export default reducer;
