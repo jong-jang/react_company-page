e7da011451afb28a458594fdc62ea21c32cf19b5
jong-jang <jjh0299@gmail.com>
2023-08-12 10:58:20 +0900

    이미지

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -3,10 +3,10 @@ import { memo } from 'react';
 function Visual() {
 	return (
 		<div id='visual' className='myScroll'>
-			<div id="stars"></div>
-			<div id="stars2"></div>
-			<div id="stars3"></div>
-      <div className="h_box"><img src={`${process.env.PUBLIC_URL}/img/hum.png`} alt="사람" /></div>
+			<div id='stars'></div>
+			<div id='stars2'></div>
+			<div id='stars3'></div>
+			<div className='h_box'></div>
 		</div>
 	);
 }
