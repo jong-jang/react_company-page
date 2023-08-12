b78d191a5095acd818a9684afefb0aa94649d879
jong-jang <jjh0299@gmail.com>
2023-08-12 11:28:18 +0900

    첫렌더링시 빈배열 옵셔널체이닝 적용

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -6,7 +6,8 @@ function Footer() {
 		<footer>
 			<h1>JJ</h1>
 			<p>2023 JJAONG &copy; ALL RIGHTS RESERVED.</p>
-			<p>{`This company was founded by ${members[0].name} in 1995`}</p>
+			{/* 첫번째 랜더링시 members는 빈 배열이므로 콘솔오류 출력 : 옵셔널체이닝으로 해결 */}
+			<p>{`This company was founded by ${members[0]?.name} in 1995`}</p>
 		</footer>
 	);
 }
