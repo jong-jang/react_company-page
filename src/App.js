e04237de152adf6103f1bdba31ea419e24de3f10
jong-jang <jjh0299@gmail.com>
2023-08-12 11:29:53 +0900

    전역데이터 app으로 이전

--- src/App.js
+++ src/App.js
@@ -14,10 +14,36 @@ import Youtube from './components/sub/Youtube';
 
 import './scss/style.scss';
 import Menu from './components/common/Menu';
-import { useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import { setYoutube, setMembers } from './redux/action';
 
 function App() {
+	// 비동기 데이터를 전역객체에 저장하는것 자체가 여러컴포넌트에서 해당 값을 재활용하기 위함이므로
+	// data fetching자체를 특정 컴포넌트가 아닌 루트컴포넌트에서 한번에 처리해서 전역에 담아놓고
+	// 원하는 컴포넌트에 자유롭게 호출
+	const dispatch = useDispatch();
 	const menu = useRef(null);
+	const fetchYoutube = useCallback(async () => {
+		const key = 'AIzaSyAsfSiK4NgpHz-QbJ48s9iUVg30JzSQ7rc';
+		const list = 'PLJkDsZz0FhUkqKafhbfEQmjhSROZ1kiIl';
+		const num = 10;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${list}&maxResults=${num}`;
+
+		const result = await axios.get(url);
+		dispatch(setYoutube(result.data.items));
+	}, [dispatch]);
+
+	const fetchMembers = useCallback(async () => {
+		const result = await axios.get(process.env.PUBLIC_URL + '/DB/members.json');
+		dispatch(setMembers(result.data.members));
+	}, [dispatch]);
+
+	useEffect(() => {
+		fetchYoutube();
+		fetchMembers();
+	}, [fetchYoutube, fetchMembers]);
 
 	return (
 		<>
