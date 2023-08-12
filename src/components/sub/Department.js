25524992d6a06218917dac33e5645437281522f9
jong-jang <jjh0299@gmail.com>
2023-08-12 10:22:42 +0900

    전역 데이터 변경 해보기

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,9 +1,11 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Layout from '../common/Layout';
 // import { useEffect, useState } from 'react';
+import { setMembers } from '../../redux/action';
 
 function Department() {
 	// const [Mounted, setMounted] = useState(true);
+	const dispatch = useDispatch();
 	const Members = useSelector((store) => store.memberReducer.members);
 
 	// useEffect(() => {
@@ -12,6 +14,16 @@ function Department() {
 
 	return (
 		<Layout name={'Department'}>
+			<button
+				onClick={() => {
+					// 전역 데이터 변경시에는 참조형자료일때 기존 state값을 deep copy해서 변경
+					const newMebers = [...Members];
+					newMebers[0].name = 'Emma';
+					dispatch(setMembers(newMebers));
+				}}
+			>
+				대표자 이름 변경
+			</button>
 			{Members.map((member, idx) => {
 				return (
 					<article key={idx}>
