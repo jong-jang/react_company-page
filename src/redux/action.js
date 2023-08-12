a769e2850930bb40fb982ef086dbb531450ce257
jong-jang <jjh0299@gmail.com>
2023-08-12 09:45:34 +0900

    action 생성함수, 전역데이터 변형자 함수 추가

---
+++ src/redux/action.js
@@ -0,0 +1,9 @@
+// 인수로 전달된 값을 payload에 담아서 액션객체를 반환하는 함수를 export
+// 해당 함수를 추후 컴포넌트에서 호출될 예정
+
+export const setMembers = (data) => {
+	return {
+		type: 'SET_MEMBERS',
+		payload: data,
+	};
+};
---
+++ src/redux/reducer.js
@@ -0,0 +1,50 @@
+import { combineReducers } from 'redux';
+
+const initMember = {
+	members: [
+		{
+			name: 'Julia',
+			position: 'President',
+			pic: 'member1.jpg',
+		},
+		{
+			name: 'David',
+			position: 'Vice President',
+			pic: 'member2.jpg',
+		},
+		{
+			name: 'Emily',
+			position: 'UI Designer',
+			pic: 'member3.jpg',
+		},
+		{
+			name: 'Paul',
+			position: 'Front-end Engineer',
+			pic: 'member4.jpg',
+		},
+		{
+			name: 'Sara',
+			position: 'Back-end Engineer',
+			pic: 'member5.jpg',
+		},
+		{
+			name: 'Michael',
+			position: 'Project Manager',
+			pic: 'member6.jpg',
+		},
+	],
+};
+
+// 초기 state값을 지정하고 추후 action객체가 넘어오면 action타입에 따라 전역 state값을 변경해주는 변형자 함수
+const memberReducer = (state = initMember, action) => {
+	switch (action.type) {
+		case 'SET_MEMBERS':
+			return { ...state, members: action.payload };
+		default:
+			return state;
+	}
+};
+
+// 해당 변형자함수가 반환하는 리턴값을 하나의 객체로 합쳐서 외부로 export
+const reducer = combineReducers({ memberReducer });
+export default reducer;
