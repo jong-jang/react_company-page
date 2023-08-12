ed9c34c3f7ad6bbd4b4d1b22833b6d23c86d4a50
jong-jang <jjh0299@gmail.com>
2023-08-12 11:28:32 +0900

    youtube 적용

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,9 +1,19 @@
 import { memo } from 'react';
+import { useSelector } from 'react-redux';
 
 function Vids() {
+	const { youtube } = useSelector((store) => store.youtubeReducer);
 	return (
 		<section id='vids' className='myScroll'>
-			Vids
+			<h1>youtube</h1>
+			{youtube.map((vid, idx) => {
+				if (idx >= 3) return null;
+				return (
+					<article key={idx}>
+						<h2>{vid.snippet.title}</h2>
+					</article>
+				);
+			})}
 		</section>
 	);
 }
