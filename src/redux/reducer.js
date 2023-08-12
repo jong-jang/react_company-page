0fbe9fc244f5133c1ebee143a197da0f8fdf75c7
jong-jang <jjh0299@gmail.com>
2023-08-12 11:29:42 +0900

    정적데이터 app.js로 이전

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,42 +1,7 @@
 import { combineReducers } from 'redux';
 
-const initMember = {
-	members: [
-		{
-			name: 'Julia',
-			position: 'President',
-			pic: 'member1.jpg',
-		},
-		{
-			name: 'David',
-			position: 'Vice President',
-			pic: 'member2.jpg',
-		},
-		{
-			name: 'Emily',
-			position: 'UI Designer',
-			pic: 'member3.jpg',
-		},
-		{
-			name: 'Paul',
-			position: 'Front-end Engineer',
-			pic: 'member4.jpg',
-		},
-		{
-			name: 'Sara',
-			position: 'Back-end Engineer',
-			pic: 'member5.jpg',
-		},
-		{
-			name: 'Michael',
-			position: 'Project Manager',
-			pic: 'member6.jpg',
-		},
-	],
-};
-
 //초기 state값을 지정하고 추후 action객체가 넘어오면 action타입에 따라 전역 state값을 변경해주는 변형자 함수
-const memberReducer = (state = initMember, action) => {
+const memberReducer = (state = { members: [] }, action) => {
 	switch (action.type) {
 		case 'SET_MEMBERS':
 			return { ...state, members: action.payload };
