16524f2a884306ee5d2a22f16160d7704d0eb9ef
jong-jang <jjh0299@gmail.com>
2023-08-12 10:11:48 +0900

    전역데이터 푸터에 호출

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Footer() {
+	const { members } = useSelector((store) => store.memberReducer);
 	return (
 		<footer>
 			<h1>JJ</h1>
 			<p>2023 JJAONG &copy; ALL RIGHTS RESERVED.</p>
+			<p>{`This company was founded by ${members[0].name} in 1995`}</p>
 		</footer>
 	);
 }
