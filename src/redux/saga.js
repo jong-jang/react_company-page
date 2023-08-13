a4e15fe2813a81adbdfd2bcba2f3c051d4788ca9
jong-jang <jjh0299@gmail.com>
2023-08-13 10:08:56 +0900

    제너레이터 함수로 saga 생성

--- src/redux/saga.js
+++ src/redux/saga.js
@@ -7,4 +7,31 @@
   call: saga에서 api관련 함수 호출 (첫번째 인수:fetching함수, 두번째 인수: fetching함수의 인수로 전달)
   fork: 위에 함수들을 최종적으로 호출
   all: 여러가지 비동기 관련 saga함수를 병렬식으로 모두 호출
+
+  작업호출
+  1- 리듀서가 컴포넌트로부터 초기 action요청을 받으면 takeLatest로 해당 요청을 가져오는 함수를 정의
+  2- api.js로 부터 fetching 함수를 가져와서 call로 호출하는 함수 정의
+  3- 데이터 응답 성공유무에 따라서 서로 다른 액션객체를 생성해서 반환
+  4- 이렇게 만들어진 한번에 호출해주는 함수 (fork, all)
+  5- 위의 함수들을 saga단에서 단계에 맞게 동기화 호출할 수 있도록 제너레이터 함수로 제작
 */
+
+import { takeLatest, put, call, fork, all } from 'redux-saga/effects';
+import { fetchYoutube } from './api';
+import * as types from './actionType';
+
+function* callYoutube() {
+	yield takeLatest(types.YOUTUBE.start, returnYoutube);
+}
+function* returnYoutube() {
+	try {
+		const response = yield call(fetchYoutube);
+		yield put({ type: types.YOUTUBE.success, payload: response.data.items });
+	} catch (err) {
+		yield put({ type: types.YOUTUBE.fail, payload: err });
+	}
+}
+
+export default function* rootSaga() {
+	yield all([fork(callYoutube)]);
+}
