292b684c7669873a9d180918c6f124e1ed402a78
jong-jang <jjh0299@gmail.com>
2023-08-13 11:29:24 +0900

    갤러리 전역데이터로 pics에 미리보기 출력

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -1,11 +1,15 @@
-import { memo } from 'react';
+import { useSelector } from 'react-redux';
 
 function Pics({ Scrolled, Pos }) {
+	const { flickr } = useSelector((store) => store.flickrReducer);
 	return (
 		<section id='pics' className='myScroll'>
 			<h1 style={{ transform: `translateX(${Scrolled - Pos}px)` }}>FLICKR</h1>
+			{flickr.map((pic, idx) => {
+				return <img key={idx} src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={pic.title} />;
+			})}
 		</section>
 	);
 }
 
-export default memo(Pics);
+export default Pics;
