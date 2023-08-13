dcc62b87efc5ade7e1ee3743d7ebfebffbd9c78a
jong-jang <jjh0299@gmail.com>
2023-08-13 09:37:05 +0900

    youtube 데이터 fetch함수 추가

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,1 +1,11 @@
 // 외부 비동기 데이터를 fetching할 함수 모음 파일
+import axios from 'axios';
+
+export const fetchYoutube = async () => {
+	const key = 'AIzaSyAsfSiK4NgpHz-QbJ48s9iUVg30JzSQ7rc';
+	const list = 'PLJkDsZz0FhUkqKafhbfEQmjhSROZ1kiIl';
+	const num = 10;
+	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${list}&maxResults=${num}`;
+
+	return await axios.get(url);
+};
