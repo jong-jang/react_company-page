6acdb6c1c16212d06547578306dc6fd367181eb1
jong-jang <jjh0299@gmail.com>
2023-08-19 11:19:49 +0900

    콘솔제거

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,9 +1,6 @@
 import { memo } from 'react';
-import { useSelector } from 'react-redux';
 
 function Vids() {
-	const { data } = useSelector((store) => store.youtube);
-	console.log(data);
 	return (
 		<section id='vids' className='myScroll'>
 			Vids
