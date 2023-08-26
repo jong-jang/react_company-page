0267d52086690153b20e853e38a2fc6ae89a1c97
jong-jang <jjh0299@gmail.com>
2023-08-26 10:35:09 +0900

    react-query로 데이터 출력

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -1,11 +1,18 @@
-import { memo } from 'react';
+import { useFlickrQuery } from '../../hooks/useFlickr';
 
 function Pics({ Scrolled, Pos }) {
+	const { data, isSuccess } = useFlickrQuery({ type: 'user', user: '198837106@N07' });
+
 	return (
 		<section id='pics' className='myScroll'>
 			<h1 style={{ transform: `translateX(${Scrolled - Pos}px)` }}>FLICKR</h1>
+			{isSuccess &&
+				data.map((el, idx) => {
+					if (idx >= 5) return null;
+					return <p key={idx}>{el.title}</p>;
+				})}
 		</section>
 	);
 }
 
-export default memo(Pics);
+export default Pics;
