0c8fdeaac1dd4703307d784af05ba150e07c7c4b
jong-jang <jjh0299@gmail.com>
2023-08-26 09:50:48 +0900

    react-query 구조로 변경

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,33 +1,24 @@
 import Layout from '../common/Layout';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useDepartmentQuery } from '../../hooks/useDepartment';
 
 function Department() {
-	const [Members, setMembers] = useState([]);
-	const [Mounted, setMounted] = useState(true);
-
-	useEffect(() => {
-		axios.get(`${process.env.PUBLIC_URL}/DB/members.json`).then((data) => {
-			Mounted && setMembers(data.data.members);
-		});
-
-		return () => setMounted(false);
-	}, [Mounted]);
+	const { data: Members, isSuccess } = useDepartmentQuery();
 
 	return (
 		<Layout name={'Department'}>
-			{Members.map((member, idx) => {
-				return (
-					<article key={idx}>
-						<div className='pic'>
-							<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
-							<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
-						</div>
-						<h2>{member.name}</h2>
-						<p>{member.position}</p>
-					</article>
-				);
-			})}
+			{isSuccess &&
+				Members.map((member, idx) => {
+					return (
+						<article key={idx}>
+							<div className='pic'>
+								<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+								<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+							</div>
+							<h2>{member.name}</h2>
+							<p>{member.position}</p>
+						</article>
+					);
+				})}
 		</Layout>
 	);
 }
