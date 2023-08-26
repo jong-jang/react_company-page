f07f2fca35d8033f5ebd696c2e76478778ad03f1
jong-jang <jjh0299@gmail.com>
2023-08-26 11:35:12 +0900

    context api를 활용한 전역관리 커스텀 훅 생성 및 적용

---
+++ src/hooks/useGlobalContext.js
@@ -0,0 +1,16 @@
+import { createContext, useContext, useState } from 'react';
+
+// 전역 컨텍스트 생성
+export const GlobalContext = createContext();
+
+// 전역으로 관리할 State값을 App에 전달해주는 커스텀 Provider 컴포넌트
+export function GlobalProvider({ children }) {
+	const [MenuOpen, setMenuOpen] = useState(false);
+	return <GlobalContext.Provider value={(MenuOpen, setMenuOpen)}>{children}</GlobalContext.Provider>;
+}
+
+// 특정 컴포넌트에 전역 컨텍스트의 값을 자유롭게 호출하도록 useContext 훅을 커스텀훅으로 내보냄
+export function useGlobalData() {
+	const globalConetext = useContext(GlobalContext);
+	return globalConetext;
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import { GlobalProvider } from './hooks/useGlobalContext';
 
 ReactDOM.render(
 	<HashRouter>
-		<App />
+		<GlobalProvider>
+			<App />
+		</GlobalProvider>
 	</HashRouter>,
 	document.getElementById('root')
 );
